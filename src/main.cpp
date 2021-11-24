d798eccb182ef6a6e42dfb49420d7d39aae20e23
sousajo <sousajo@pop-os.localdomain>
2021-11-24 12:06:45 +0000

    test: add test input files

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <sstream>
+#include <chrono>
+
+#include "dijkstra.hpp"
+#include "fileHandler.hpp"
+
+int main([[maybe_unused]] int argc, char * argv[]) {
+#if defined(__linux__)
+    auto start = std::chrono::steady_clock::now();
+#endif
+    auto fileHandler = File(argv[1]);
+    Graph graph{fileHandler.readInput()};
+    std::vector solution = graph.dijkstra();
+    fileHandler.writeOutput(solution);
+#if defined(__linux__)
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+#endif
+    return 0;
+}
