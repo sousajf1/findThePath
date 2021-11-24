18ad2055ad5db99b7ff269977f0a4e4a2c8f7128
sousajo <sousajo@pop-os.localdomain>
2021-11-24 12:02:46 +0000

    global: little refacto and added fileHandler

--- main.cpp
+++ main.cpp
@@ -2,30 +2,20 @@
 #include <chrono>
 
 #include "dijkstra.hpp"
+#include "fileHandler.hpp"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char * argv[]) {
+int main([[maybe_unused]] int argc, char * argv[]) {
+#if defined(__linux__)
     auto start = std::chrono::steady_clock::now();
-    std::fstream fileStream;
-    constexpr char const *inExtension = ".cav";
-    constexpr char const *outExtension = ".csn";
-
-    auto fileToOpen = std::string{argv[1]} + std::string{inExtension};
-    auto fileToWrite = std::string{argv[1]} + std::string{outExtension};
-
-    fileStream.open(fileToOpen, std::ios_base::in);
-    std::ostringstream os;
-    os << fileStream.rdbuf();
-    std::string input = os.str();
-
-    Graph graph{input};
+#endif
+    auto fileHandler = File(argv[1]);
+    Graph graph{fileHandler.readInput()};
     std::vector solution = graph.dijkstra();
-
-    std::ofstream outFile(fileToWrite);
-    for (auto const &s : solution) {
-        outFile << s << " ";
-    }
+    fileHandler.writeOutput(solution);
+#if defined(__linux__)
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+#endif
     return 0;
 }
